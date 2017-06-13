97a507fb8195827ddd68eb20ab3bd2ba5cba5623
Amit Karnik <amkarnik@cisco.com>
2017-06-13 12:15:10 +0530

    REST APIs

--- src/com/restBankApp/Account.java
+++ src/com/restBankApp/Account.java
@@ -1,16 +1,69 @@
 package com.restBankApp;
 
+import java.util.*;
+
+
 public class Account {
+	/*
+	 * JAVA BEAN class for Bank Account
+	 */
 	private int accId = 0;
 	private String name = "";
 	private double balance = 0;
+	private double phoneNumber = 0;
+	private int transactionID = 100;
+	private Map<Integer, String> history = new HashMap<Integer, String>();
 
-	public Acconut(int accId, String name, double balance){
-		super();
+	public Account(Account accountDetails) {
 	}
 
 	public int getAccId(){
 		return this.accId;
+	}
+
+	public void setAccId(int accId){
+		this.accId = accId;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setPhoneNumber(double phNumber){
+		this.phoneNumber = phNumber;
+	}
+
+	public void setBalance(double bal, String action){
+		this.transactionID += 1;
+		if(action == "add"){
+			String msg = "Deposited " + bal;
+			this.balance += bal;
+			this.history.put(this.transactionID, msg);
+		}else{
+			String msg = "Withdrawn " + bal;
+			this.balance -= bal;
+			this.history.put(this.transactionID, msg);
+		}
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public double getBalance(){
+		return this.balance;
+	}
+
+	public double getPhoneNumber(){
+		return this.phoneNumber;
+	}
+
+	public Map<Integer, String> getHistory(){
+		return this.history;
+	}
+
+	public Account getAccountDetails(){
+		return this;
 	}
 
 }
--- src/com/restBankApp/BankApp.java
+++ src/com/restBankApp/BankApp.java
@@ -12,13 +12,52 @@ import javax.ws.rs.core.Response;
 
 @Path("/bank")
 public class BankApp {
+	private int defaultAccNumber = 123;
+
 	@POST
 	@Path("/account")
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Response newAccount(Account accountDetails){
+		/*
+		 * API to Create new Bank Account
+		 */
 		Account newAcc = new Account(accountDetails);
+		int accNumber = this.defaultAccNumber ++;
+		newAcc.setAccId(accNumber);
+		return Response.ok().entity(accNumber).build();
+	}
+
+	@POST
+	@Path("/account/{id}/")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response transaction(@QueryParam("action") String action, @QueryParam("funds") double funds,
+			@PathParam("id") int accId){
+		/*
+		 * API for BANK transactions - Deposit/Withdraw
+		 */
 		return Response.ok().build();
 	}
-	
+
+	@GET
+	@Path("/account/transsactions/{id}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getAccHistory(@PathParam("id") int accId){
+		/*
+		 * API To get the Transaction HIstory of a Account
+		 */
+		// get the account
+		// get the history of transaction of that account
+		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/account/{id}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getAccDetails(@PathParam("id") int accId){
+		/*
+		 * API to get the Account details
+		 */
+		return Response.ok().build();
+	}
 
 }
